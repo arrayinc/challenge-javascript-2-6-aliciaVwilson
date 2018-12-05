923fc3de28e15157f7b845ea9efd760ab54b1692
Alicia Wilson <alicia@thearrayschool.com>
2018-12-05 16:14:19 -0700

    tried the quadratic formula

--- answer.js
+++ answer.js
@@ -1,1 +1,8 @@
 // *** YOUR ANSWER BELOW ***
+function quadEquationSolver(a, b, c){
+  let soln1 = (-b + Math.sqrt(b * b - 4 * a * c)) / 2 * a;
+  let soln2 = (-b - Mat.sqrt(b * b- 4 * a * c)) / 2 * a;
+  return "soln1" + "soln2";
+}
+
+quadEquationSolver(1, 4, -21);
